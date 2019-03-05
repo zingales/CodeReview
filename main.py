e28079218d71388f8ac4b80ab4edcd6cc1295b47
belle <belle@noemail.com>
2019-03-05 11:24:55 -0800

    added quitting method

--- main.py
+++ main.py
@@ -19,10 +19,26 @@ if __name__ == "__main__":
 
     env = Environment(code_path)
 
-    print("to return to working dir just type revert")
+    howto = '''
+To help you view and run Code Review locally
+How to use:
+* Enter git reference id (Hash or branch) for the code you'd like to review.
+* To return to you working directory type revert
+* To quit type exit
+    '''
+    print(howto)
     while True:
         text = input("paste reference id: ")
         if text == "revert":
             env.prepare_working_dir()
             continue
-        env.switch_to(text)
+        elif text == "exit":
+            if env.starting_branch is not None:
+                if 'y' == input("we've not retuned to the working branch exit anyway? (y/n)"):
+                    break
+            else:
+                break
+        elif text == "help":
+            print(howto)
+        else:
+            env.switch_to(text)
